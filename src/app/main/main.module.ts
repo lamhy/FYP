3025cb6702c0f0efedb015c7abc0ea474587e5fa
lamhy <lamhy1029@gmail.com>
2017-12-20 12:29:31 +0800

    remove quick panel in main.module.ts

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -11,7 +11,7 @@ import { FuseToolbarComponent } from './toolbar/toolbar.component';
 import { FuseNavigationModule } from '../core/components/navigation/navigation.module';
 import { FuseNavbarVerticalToggleDirective } from './navbar/vertical/navbar-vertical-toggle.directive';
 import { FuseNavbarHorizontalComponent } from './navbar/horizontal/navbar-horizontal.component';
-import { FuseQuickPanelComponent } from './quick-panel/quick-panel.component';
+//import { FuseQuickPanelComponent } from './quick-panel/quick-panel.component';
 import { FuseThemeOptionsComponent } from '../core/components/theme-options/theme-options.component';
 import { FuseShortcutsModule } from '../core/components/shortcuts/shortcuts.module';
 import { FuseSearchBarModule } from '../core/components/search-bar/search-bar.module';
